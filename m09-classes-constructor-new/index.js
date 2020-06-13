9073d026f062bb718e1db7155b414f507d2779be
Igor Goloborodko <igor_goloborodko@epam.com>
2020-06-13 12:31:53 +0300

    m-09 Cosmetic update

--- m09-classes-constructor-new/index.js
+++ m09-classes-constructor-new/index.js
@@ -54,16 +54,16 @@ class Vector {
     }
 }
 
-const a = new Vector([1, 2,	3]);
-const b = new Vector([3, 4,	5]);
-const c = new Vector([5, 6,	7, 8]);
-const d = new Vector([1, 2, 3]);
+// const a = new Vector([1, 2,	3]);
+// const b = new Vector([3, 4,	5]);
+// const c = new Vector([5, 6,	7, 8]);
+// const d = new Vector([1, 2, 3]);
 
 // console.log(a.add(b)); // should return a new Vector([4, 6, 8])
 // console.log(a.subtract(b)); // should return a new Vector([-2, -2, -2])
 // console.log(a.dot(b)); // should return	1*3	+ 2*4 +	3*5	= 26
 // console.log(a.norm()); // should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
 // console.log(a.add(c)); // throws an error
-console.log(a.toString()); //(1,2,3)
-console.log(a.toString() === '(1,2,3)'); //true
-console.log(a.equals(d)); //true
+// console.log(a.toString()); //(1,2,3)
+// console.log(a.toString() === '(1,2,3)'); //true
+// console.log(a.equals(d)); //true
